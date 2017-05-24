e6f22af402827264da7a7d23a757ee2be48da8ce
mykola.khazanovych <nickhaz1981@gmai.com>
2017-05-24 14:45:55 +0300

    FindServlet.java has been simplified!

--- src/com/artbox/servlet/FindServlet.java
+++ src/com/artbox/servlet/FindServlet.java
@@ -1,6 +1,7 @@
 package com.artbox.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.artbox.model.ArtBox;
 import com.artbox.storage.ArtBoxStorage;
-import com.artbox.util.ParamUtils;
 
 @WebServlet("/find")
 public class FindServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 485135717800530684L;
 	private static final String ART_BOX_THEME = "theme";
+
 	public FindServlet() {
 		super();
 	}
@@ -24,25 +25,19 @@ public class FindServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		
 		String theme = request.getParameter(ART_BOX_THEME);
 
-		if (Validator.isBlank(theme)) {
-			response.getWriter().append("Please enter non-null/non-empty input value of request parameter!");
-			response.flushBuffer();
-		} else {
-			ArtBoxStorage storage = ArtBoxStorage.getInstance();
-			ArtBox findArtBox = storage.findByTheme(theme);
+		ArtBoxStorage storage = ArtBoxStorage.getInstance();
+		ArtBox findArtBox = storage.findByTheme(theme);
 
-			if (findArtBox != null) {
-					response.getWriter().append("ArtBox with theme \"" + theme +
-							"\" has been find!" + "</p>" + findArtBox.toString());
-					response.flushBuffer();
-			} else {
-					response.getWriter().append("ERROR! There is no ArtBox with theme \"" + theme + "\" in the storage!");
-					response.flushBuffer();
-			}
+		String message = "ERROR! There is no ArtBox with entered theme \'" + theme + "\' in the storage!";
+		if (findArtBox != null) {
+			message = "<i>" + "ArtBox with theme \"" + theme + "\" has been find!" + "</i>" + "<br>"
+					+ findArtBox.toString();
 		}
+
+		PrintWriter out = response.getWriter();
+		out.println(message);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
