46966c59a44f662836d3a4a43ebb9780846f0673
mykola.khazanovych <nickhaz1981@gmai.com>
2017-05-24 13:46:30 +0300

    AddServlet.java was finally simplified.

--- src/com/artbox/servlet/AddServlet.java
+++ src/com/artbox/servlet/AddServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.artbox.util.ParamUtils;
 import com.artbox.model.ArtBox;
 import com.artbox.storage.ArtBoxStorage;
 
@@ -29,44 +28,35 @@ public class AddServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		
 		String theme = request.getParameter(ART_BOX_THEME);
 		String ageStr = request.getParameter(ART_BOX_RECOMMENDED_AGE);
 		String costStr = request.getParameter(ART_BOX_COST);
-		
+
 		String message;
-	
-		
+
 		try {
-			//TODO: Check whether parse method validate null and empty value(s)
+
 			int age = Integer.parseInt(ageStr);
 			float cost = Float.parseFloat(costStr);
+
+			ArtBoxStorage artboxStorage = ArtBoxStorage.getInstance();
+			ArtBox artBox = new ArtBox(theme, age, cost);
 			
-			boolean isValid = ParamUtils.isNotBlank(theme, age, cost);
-			
-			if(isValid) {
-				
-				message = "Error! Artbox can't be added!";
-				
-				ArtBoxStorage artboxStorage = ArtBoxStorage.getInstance();
-				ArtBox artBox = new ArtBox(theme, age, cost);
-				
-				if(artboxStorage.add(artBox)) {
-					
-					message = "Success! Artbox has been added!";
-				}
-				
-			} else {
-				message = "Error! Parametrs can't be empty or zero!";
+			message = "Error! Artbox can't be added!";
+
+			if (artboxStorage.add(artBox)) {
+
+				message = "Success! Artbox has been added!";
 			}
 			
 		} catch (NumberFormatException nfe) {
-			message = "Error! Number format error!";
+			
+			message = "Error! Number format error! Please enter correct values for ArtBox'es 'theme', 'age' and 'cost'!";
 		}
-		
+
 		PrintWriter out = response.getWriter();
 		out.println(message);
-		
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
